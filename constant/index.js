3757a01c3cd2c93344ea4fe2a06cd886f23c91b6
rakhaallea <rakhaallea@gmail.com>
2025-12-01 23:53:08 +0700

    setup init

---
+++ constant/index.js
@@ -0,0 +1,206 @@
+import parfum1 from '@/public/img/1.png';
+import parfum2 from '@/public/img/2.png';
+import parfum3 from '@/public/img/3.png';
+import parfum4 from '@/public/img/4.png';
+
+import model from '@/public/img/model.png';
+import banner from '@/public/img/banner.jpg';
+
+const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "shopAll",
+        title: "shop all",
+    },
+    {
+        id: "bestsellers",
+        title: "bestsellers",
+    },
+    {
+        id: "perfumes",
+        title: "perfumes",
+    },
+    {
+        id: "About",
+        title: "About Us",
+    },
+    {
+        id: "blog",
+        title: "Blog",
+    },
+];
+
+const buttonBestSellers = ["Bellavita", "Bellavita", "Bellavita", "Bellavita", "Bellavita", "Bellavita",]
+
+const bestSellers = [
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+    {
+        img: parfum2,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'A Scent for midnight souls-floral, spicy, and hyptonic.',
+        size: '50ml',
+        price: 'Rp.100.000,00'
+    },
+]
+
+const auraParfum = [
+    {
+        img: parfum4,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'Confident. Grounded. Magnetic.Explore deep, woody, and spicy compositionscrafted  to leave a bold, lasting impression.',
+        cta: 'Browse Men’s Scents',
+    },
+    {
+        img: parfum4,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'Confident. Grounded. Magnetic.Explore deep, woody, and spicy compositionscrafted  to leave a bold, lasting impression.',
+        cta: 'Browse Men’s Scents',
+    },
+    {
+        img: parfum4,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'Confident. Grounded. Magnetic.Explore deep, woody, and spicy compositionscrafted  to leave a bold, lasting impression.',
+        cta: 'Browse Men’s Scents',
+    },
+    {
+        img: parfum4,
+        title: 'Moonlit Flour - Eau de Parfum',
+        desc: 'Confident. Grounded. Magnetic.Explore deep, woody, and spicy compositionscrafted  to leave a bold, lasting impression.',
+        cta: 'Browse Men’s Scents',
+    },
+]
+
+const singleParfum = [
+    {
+        img: parfum1,
+        title: 'Aura for Everyone',
+        subTitle: 'A scent for every soul, every story',
+        desc: 'A hypnotic blend oif night-blooming jasmine, warm amber, and exotic spices. Perfect for the mysterious and alluring.',
+        disc: 'Rp.85..000,00',
+        price: 'Rp.100.000,00'
+    }
+]
+
+const blogParfum = [
+    {
+        date: 'Oktober 15, 2025',
+        title: 'The Art of Layering Fragrances',
+        desc: 'Discover the secrets of combining different scents to create your unique signature frafrance that evolves throughout the day.',
+        cta: 'Learn more'
+    },
+    {
+        date: 'Oktober 15, 2025',
+        title: 'The Art of Layering Fragrances',
+        desc: 'Discover the secrets of combining different scents to create your unique signature frafrance that evolves throughout the day.',
+        cta: 'Learn more'
+    },
+]
+
+const faq = [
+    {
+        title: 'What is Webflow and why is it the best website builder?',
+        desc: 'A hypnotic blend oif night-blooming jasmine, warm amber, and exotic spices. Perfect for the mysterious and alluring.'
+    },
+    {
+        title: 'What is Webflow and why is it the best website builder?',
+        desc: 'A hypnotic blend oif night-blooming jasmine, warm amber, and exotic spices. Perfect for the mysterious and alluring.'
+    },
+    {
+        title: 'What is Webflow and why is it the best website builder?',
+        desc: 'A hypnotic blend oif night-blooming jasmine, warm amber, and exotic spices. Perfect for the mysterious and alluring.'
+    },
+    {
+        title: 'What is Webflow and why is it the best website builder?',
+        desc: 'A hypnotic blend oif night-blooming jasmine, warm amber, and exotic spices. Perfect for the mysterious and alluring.'
+    },
+]
+
+const openingHours = [
+    {
+        day: 'Monday - Friday',
+        time: '10:00 am - 10:00 pm'
+    },
+    {
+        day: 'Saturday',
+        time: '11:00 am - 10:00 pm'
+    },
+    {
+        day: 'Sunday',
+        time: '12:00 am - 10:00 pm'
+    },
+]
+
+const listShop = ['New Arrivals', 'Best Sellers', 'Gift Sets', 'Sample Sets', 'Sale'
+]
+
+const listServices = ['Orders', 'Details', 'Return', 'FAQs'
+]
+
+const socials = [
+    {
+        name: "Instagram",
+        icon: "ri-instagram-line",
+        url: "#",
+    },
+    {
+        name: "X (Twitter)",
+        icon: "ri-twitter-x-line",
+        url: "#",
+    },
+    {
+        name: "Facebook",
+        icon: "ri-facebook-circle-fill",
+        url: "#",
+    },
+    {
+        name: "Whatsapp",
+        icon: "ri-whatsapp-line",
+        url: "#",
+    },
+];
+
+export { navLinks, buttonBestSellers, auraParfum, bestSellers, singleParfum, blogParfum, faq, openingHours, listShop, listServices, socials }
