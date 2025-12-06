464e3a1c773443616d331413da5cc15efcef2554
rakhaallea <rakhaallea@gmail.com>
2025-12-06 19:31:57 +0700

    fixed navbar lg

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                     </button>
                 </div>
             </div>
-            <ul className={`absolute top-28 right-4 lg:static z-99 flex-col gap-4 lg:flex-row lg:flex justify-between items-center w-1/2 mx-auto py-6 tracking-widest capitalize bg-[#252726] lg:bg-transparent lg:translate-y-0 transition-all duration-300 lg:transition-none ease-in-out ${!isActive ? "opacity-10 lg:opacity-100 translate-x-250" : "opacity-100 translate-0"}`}>
+            <ul className={`absolute top-28 right-4 lg:static z-99 flex-col gap-4 lg:flex-row lg:flex justify-between items-center w-1/2 mx-auto py-6 tracking-widest capitalize bg-[#252726] lg:bg-transparent lg:translate-y-0 transition-all duration-300 lg:transition-none ease-in-out ${!isActive ? "opacity-10 lg:opacity-100 translate-x-250 lg:translate-x-0" : "opacity-100 translate-0"}`}>
                 {navLinks.map((nav) => (
                     <li key={nav.id} className="px-6 py-2 lg:px-0 lg:py-0 hover:bg-[#9B7E5B] lg:hover:bg-transparent lg:hover:text-[#9B7E5B]"><Link href={nav.id}>{nav.title}</Link></li>
                 ))}
